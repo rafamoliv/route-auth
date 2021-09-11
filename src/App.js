61a64d7a6c3c94f9ad8dce231b5c41a8b6351a5c
rafamoliv <rafaoliv.ti@gmail.com>
2021-09-11 18:29:58 -0300

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
+import Routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>wee</h1>
-    </div>
-  );
-}
+const App = () => <Routes />;
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Hi</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
